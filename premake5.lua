74e62024567ed42e68b5e8eb613d3f605f6e3683
DidaS <didasoficial@gmail.com>
2023-07-26 19:28:46 +0100

    Init
    Following the [RayTracing series](https://www.youtube.com/watch?v=gfW1Fhd9u9Q&list=PLlrATfBNZ98edc5GshdBtREv5asFW3yXl)

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "RTE"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "RTE"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "RTE"
